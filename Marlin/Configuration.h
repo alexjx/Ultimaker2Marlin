ff2d09f8c4b315b4d0eca93cecd44e93debef1a9
Jia Xin <alexjx@gmail.com>
2019-01-31 10:55:52 +0800

    smaller the temperature window

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -163,9 +163,9 @@
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
 // Actual temperature must be close to target for this long before M109 returns success
-#define TEMP_RESIDENCY_TIME 750 // (milliseconds)
-#define TEMP_HYSTERESIS 3       // (degC) range of +/- temperatures considered "close" to the target one
-#define TEMP_WINDOW     2       // (degC) Window around target to start the residency timer x degC early.
+#define TEMP_RESIDENCY_TIME 850 // (milliseconds)
+#define TEMP_HYSTERESIS 2       // (degC) range of +/- temperatures considered "close" to the target one
+#define TEMP_WINDOW     1       // (degC) Window around target to start the residency timer x degC early.
 
 // The minimal temperature defines the temperature below which the heater will not be enabled It is used
 // to check that the wiring to the thermistor is not broken.
